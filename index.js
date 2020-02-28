5d6c900b2bf0af13cf9409c646568085b147ac99
Brandon Weaver <a7xncob@gmail.com>
2020-02-28 21:22:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function hoursWorkedOnDate(date) {
 }
 
 function wagesEarnedOnDate(date) {
-  return this.hoursWorkedOnDate(date) * this.payPerHour;
+  return hoursWorkedOnDate(this, date) * this.payPerHour;
 }
 
 /*
