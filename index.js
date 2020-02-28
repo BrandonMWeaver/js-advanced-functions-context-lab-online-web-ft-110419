c884d00fa73a80df4a6bf5594b8640d866634b18
Brandon Weaver <a7xncob@gmail.com>
2020-02-28 21:17:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ function createTimeOutEvent(dateTime) {
   return this;
 }
 
+function hoursWorkedOnDate(date) {
+  
+}
+
 /*
 We're giving you this function. Take a look at it, you might see some usage
 that's new and different. That's because we're avoiding a well-known, but
