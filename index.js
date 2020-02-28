f5807d03e221ea1b1260897761bb2e025c255e6e
Brandon Weaver <a7xncob@gmail.com>
2020-02-28 21:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,13 @@
  */
 
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
-}
+  let eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
+  
+  let payable = eligibleDates.reduce(function (memo, d) {
+    return memo + wagesEarnedOnDate.call(this, d);
+  }.bind(this), 0); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  
+  return payable;
+};
